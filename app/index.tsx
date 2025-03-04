f50576217f38e6cea05e7dfe2e05aed9be907c37
StevoEs <stefan-studenski@live.de>
2025-03-04 09:40:50 +0100

    index weiterleitung hinzugefügt

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,32 @@
-import { StyleSheet, Text, View } from "react-native";
 import { registerRootComponent } from 'expo';
+import { useEffect } from 'react';
+import { View, StyleSheet } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
 import App from '../App';
 
-registerRootComponent(App);
+// Splash Screen verhindern, bis die App geladen ist
+SplashScreen.preventAutoHideAsync();
 
+function Main() {
+  useEffect(() => {
+    const hideSplash = async () => {
+      await SplashScreen.hideAsync();
+    };
+    
+    hideSplash();
+  }, []);
 
+  return (
+    <View style={styles.container}>
+      <App />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+registerRootComponent(Main);
